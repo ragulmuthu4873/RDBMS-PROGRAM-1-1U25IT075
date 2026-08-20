f439a665218af39007e28d2f0899faba792e8a84
RAGUL M <ragulmuthu38@gmail.com>
2026-08-20 13:45:05 +0530

    RDBMS PROGRAM-1

--- solution.sql
+++ solution.sql
@@ -2,7 +2,7 @@ CREATE DATABASE UniversityDB;
 USE UniversityDB;
 CREATE TABLE Department(
   DepartmentID numeric (5) PRIMARY KEY,
-  DepartmentName varchar(20) NOT NULL,
-  HOD varchar (20) NOT NULL
+  DepartmentName varchar(20) ,
+  HOD varchar (20) 
 );
-  desc Department;
+  desc Department
